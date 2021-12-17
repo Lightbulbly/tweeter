2e71c8b2828b9c2276f3056c220cc111b5a09379
Noya Onggud <yanuo44@gmail.com>
2021-12-17 17:27:29 -0500

    clean up the code

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -10,19 +10,6 @@
 // A $( document ).ready() block.
 
 $(document).ready(function() {
-  // console.log("ready!");
-
-  //   const tweetData = {
-  //     "user": {
-  //       "name": "Newton",
-  //       "avatars": "https://i.imgur.com/73hZDYK.png",
-  //       "handle": "@SirIsaac"
-  //     },
-  //     "content": {
-  //       "text": "If I have seen further it is by standing on the shoulders of giants"
-  //     },
-  //     "created_at": 1461116232227
-  //   };
   
   const createTweetElement = function(tweet) {
     const escape = function(str) {
@@ -34,8 +21,7 @@ $(document).ready(function() {
     //replace <p>${tweet.content.text}</p> with safeHTML
 
     const $tweet = $(`
-    
-    <article>
+        <article>
           <header class="all-tweets-header">
             <div class="user-avatar-and-username">
                           <div class="avatar"><img src="${tweet.user.avatars}"></div>
@@ -45,10 +31,8 @@ $(document).ready(function() {
           </header>
           <div class="tweet-text">
           <p>${safeHTML}</p>
-
         </div>
             <footer>
-              
               <div class="date-posted">${timeago.format(tweet.created_at)}</div>
 
               <div class="flag-retweet-like-icons">
@@ -67,24 +51,15 @@ $(document).ready(function() {
               </div>
 </footer>
         </article>
-
     `);
 
     return $tweet;
   
   };
-
-  // const $tweet = createTweetElement(tweetData);
-
-  // Test / driver code (temporary)
-  // console.log("here is", $tweet); // to see what it looks like
-  // $('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
  
   const renderTweets = (tweets)=>{
     $('#tweets-container').html(``);//empty container and loop again
-    // console.log("tweets:",tweets);
     for (const tweet of tweets) {
-      // console.log("tweet:",tweet);
       const newTweet = createTweetElement(tweet);
       $('#tweets-container').prepend(newTweet);
     }
@@ -94,55 +69,36 @@ $(document).ready(function() {
     $.ajax({
       type: 'GET',
       url: "/tweets/",
-      // contentType: false,
-      // processData:false,//this is a must
       success: function(response) {
-        // console.log("success!");
-        // console.log("response GET /tweets:",response);
-        // $('#tweets-container').html(renderTweets(response));
         renderTweets(response);
-
       }
-      // window.location.reload();
-
     });
   };
 
   $("#post-new-tweet").submit(function(event) {
     event.preventDefault();
-    // console.log("here!",$("#tweet-text").val());
     if ($("#tweet-text").val().length === 0) {
-      // alert("tweet cannot be empty");
       $('#error-message-text-limit').hide();
       $('#error-message-empty-tweet').show();
       return;
     }
     if ($("#tweet-text").val().length > 140) {
-      // alert("tweet exceeds 140 characters");
       $('#error-message-empty-tweet').hide();
       $('#error-message-text-limit').show();
       return;
     }
     console.log("post-new-tweet", $("#post-new-tweet"));
     $.ajax({
-      // type: "POST",
       method:"POST",
       url: "/tweets",
       data: $("#post-new-tweet").serialize(),
       // dataType: "json",//this is what was causing the problem
       // encode: true,
     }).done(function() {
-      // console.log("data:",data);
       loadtweets();
     });
-
-
   });
-
-
   loadtweets();
-
-
 });
 
 
