15c17ab409a801a17249db0e63cf5d62c15a61c3
Noya Onggud <yanuo44@gmail.com>
2021-12-17 09:26:17 -0500

    add XSS

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -12,19 +12,27 @@
 $(document).ready(function() {
   // console.log("ready!");
 
-  const tweetData = {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  };
+  //   const tweetData = {
+  //     "user": {
+  //       "name": "Newton",
+  //       "avatars": "https://i.imgur.com/73hZDYK.png",
+  //       "handle": "@SirIsaac"
+  //     },
+  //     "content": {
+  //       "text": "If I have seen further it is by standing on the shoulders of giants"
+  //     },
+  //     "created_at": 1461116232227
+  //   };
   
   const createTweetElement = function(tweet) {
+    const escape = function(str) {
+      let div = document.createElement("div");
+      div.appendChild(document.createTextNode(str));
+      return div.innerHTML;
+    };
+    const safeHTML = `<p>${escape(tweet.content.text)}</p>`;
+    //replace <p>${tweet.content.text}</p> with safeHTML
+
     const $tweet = $(`
     
     <article>
@@ -36,7 +44,8 @@ $(document).ready(function() {
           <div class="tweeterHandle">${tweet.user.handle}</div>
           </header>
           <div class="tweet-text">
-          <p>${tweet.content.text}</p>
+          <p>${safeHTML}</p>
+
         </div>
             <footer>
               
@@ -60,18 +69,21 @@ $(document).ready(function() {
         </article>
 
     `);
+
     return $tweet;
   
   };
 
-  const $tweet = createTweetElement(tweetData);
+  // const $tweet = createTweetElement(tweetData);
 
   // Test / driver code (temporary)
   // console.log("here is", $tweet); // to see what it looks like
-  $('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+  // $('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
  
   const renderTweets = (tweets)=>{
+    // console.log("tweets:",tweets);
     for (const tweet of tweets) {
+      // console.log("tweet:",tweet);
       const newTweet = createTweetElement(tweet);
       $('#tweets-container').prepend(newTweet);
     }
@@ -85,14 +97,13 @@ $(document).ready(function() {
       // processData:false,//this is a must
       success: function(response) {
         // console.log("success!");
+        // console.log("response GET /tweets:",response);
         $('#tweets-container').html(renderTweets(response));
       }
       // window.location.reload();
 
     });
   };
-
-
 
   $("#post-new-tweet").submit(function(event) {
     event.preventDefault();
@@ -105,7 +116,7 @@ $(document).ready(function() {
       alert("tweet exceeds 140 characters");
       return;
     }
-   
+    console.log("post-new-tweet", $("#post-new-tweet"));
     $.ajax({
       type: "POST",
       url: "/tweets/",
